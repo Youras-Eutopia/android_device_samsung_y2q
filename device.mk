e6a1ae90d9b325e398b248e4c34c776ce9372fe4
DY3413 <milosix3413@gmail.com>
2025-03-26 21:19:14 +0900

    y2q: Drop unused indoorchannel.info

--- device.mk
+++ device.mk
@@ -58,7 +58,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # WiFi configs
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
     $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
